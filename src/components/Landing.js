8c9c4b130f9bcfe2031088cd47286527b292b827
Derek Garnett <drkgrntt@gmail.com>
2018-03-05 19:31:04 -0600

    rearrange some components

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react';
 import Logo from './Logo';
 import BlogWidget from './BlogWidget';
@@ -7,45 +7,102 @@ import Contact from  './Contact';
 import FacebookFeed from './FacebookFeed';
 import TwitterFeed from './TwitterFeed';
 
-const Landing = () => {
-  return (
-    <div className="body">
-      <Grid doubling stackable>
-        <Grid.Column width={12}>
-          <Grid.Row>
-            <Grid stackable>
-              <Grid.Column className="no-space" width={5}>
-                <div className="center">
-                  <Logo />
-                  <h2 className="section-title">Feature Blog Post</h2>
-                  <BlogWidget />
-                </div>
-              </Grid.Column>
-              <Grid.Column className="no-space" width={11}>
-                <h2 className="section-title">Edited Works</h2>
-                <div className="center">
-                  <EditedWorks />
-                </div>
-              </Grid.Column>
-            </Grid>
-          </Grid.Row>
-          <Grid.Row>
-            <h2 className="section-title">Contact</h2>
-            <div className="center">
-              <Contact />
-            </div>
-          </Grid.Row>
-        </Grid.Column>
-        <Grid.Column className="no-space" width={4}>
-          <h2 className="section-title">Social</h2>
+class Landing extends Component {
+  renderComputerContact() {
+    let width = window.innerWidth;
+
+    if (width > 900) {
+      return (
+        <div>
+          <h2 style={{ marginTop: '15px' }} className="section-title">Contact</h2>
           <div className="center">
-            <TwitterFeed />
-            <FacebookFeed />
+            <Contact />
           </div>
-        </Grid.Column>
-      </Grid>
-    </div>
-  );
+        </div>
+      );
+    }
+  }
+
+  renderMobileContact() {
+    let width = window.innerWidth;
+
+    if (width < 900) {
+      return (
+        <div>
+          <h2 className="section-title">Contact</h2>
+          <div className="center">
+            <Contact />
+          </div>
+        </div>
+      );
+    }
+  }
+
+  renderComputerBlog() {
+    let width = window.innerWidth;
+
+    if (width > 900) {
+      return (
+        <div>
+          <h2 className="section-title">Feature Blog Post</h2>
+          <BlogWidget />
+        </div>
+      );
+    }
+  }
+
+  renderMobileBlog() {
+    let width = window.innerWidth;
+
+    if (width < 900) {
+      return (
+        <div>
+          <h2 className="section-title">Feature Blog Post</h2>
+          <BlogWidget />
+        </div>        
+      );
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="body">
+        <Grid doubling stackable>
+          <Grid.Column width={12}>
+            <Grid.Row>
+              <Grid stackable>
+                <Grid.Column className="no-space" width={5}>
+                  <div className="center">
+                    <Logo />
+                    {this.renderComputerBlog()}
+                  </div>
+                </Grid.Column>
+                <Grid.Column className="no-space" width={11}>
+                  <h2 className="section-title">Edited Works</h2>
+                  <div className="center">
+                    <EditedWorks />
+                  </div>
+                </Grid.Column>
+                {this.renderMobileBlog()}
+              </Grid>
+            </Grid.Row>
+            <Grid.Row>
+              {this.renderComputerContact()}
+            </Grid.Row>
+          </Grid.Column>
+          <Grid.Column className="no-space" width={4}>
+            <h2 className="section-title">Social</h2>
+            <div className="center">
+              <TwitterFeed />
+              <FacebookFeed />
+            </div>
+            {this.renderMobileContact()}
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Landing;
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
       <img
         className="logo"
         width="90%"
-        src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/17757276_1475146552519573_7674930973478994129_n.png?oh=91ebe7ccfa5343e13066f4073b0f0b1a&oe=5B1F5FDE"
+        src="https://i.imgur.com/o01vg3Q.jpg"
       />
       <hr />
       <p className="text center-text">Developmental and copy editing for all forms of writing at affordable prices.</p>
